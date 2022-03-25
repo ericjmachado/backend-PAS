682938de74f24f17306f7c6b0400d96efe56376b
ericjmachado <ericmachadoeng@gmail.com>
2022-03-25 20:04:05 -0300

    Add token

--- core/views/home.py
+++ core/views/home.py
@@ -1,5 +1,11 @@
 from django.views.generic import TemplateView
+from django.conf import settings
 
 
 class HomeView(TemplateView):
     template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        context["token"] = settings.BEARER_TOKEN
+        return context
